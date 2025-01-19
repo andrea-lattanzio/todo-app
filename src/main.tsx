9d9ac9e56a94feb8709e1cc97abf72b5846ef37a
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-01-19 21:55:49 +0100

    feat: add spinner during loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import React, { Suspense } from 'react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { Suspense } from "react";
+import Spinner from "./components/Spinner.tsx";
 
 const Home = React.lazy(() => import("./Routes/Home.tsx"));
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route>
           <Route path="/" element={<App />} />
-          <Route index element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <Home />
-            </Suspense>
-          }/>
+          <Route
+            index
+            element={
+              <Suspense fallback={<Spinner />}>
+                <Home />
+              </Suspense>
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
-    <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);

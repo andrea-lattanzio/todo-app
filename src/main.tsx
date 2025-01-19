d0e962d52deccce3047d3a23da789a4ff6a47daa
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-01-19 21:43:04 +0100

    feat: add home page component and route

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React, { Suspense } from 'react'
+
+const Home = React.lazy(() => import("./Routes/Home.tsx"));
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route path="/" element={<App />} />
+          <Route index element={
+            <Suspense fallback={<div>Loading...</div>}>
+              <Home />
+            </Suspense>
+          }/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
     <App />
   </StrictMode>,
 )

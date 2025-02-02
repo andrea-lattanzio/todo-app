87809d9fb431bec191a5f02fe99701c6ba1c47ff
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-02-02 18:37:15 +0100

    feat: add query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -8,46 +8,52 @@ import Spinner from "./components/Spinner.tsx";
 import Navbar from "./components/Navbar/Navbar.tsx";
 import Sidebar from "./components/Sidebar.tsx";
 import Login from "./Routes/Login/Login.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const Home = React.lazy(() => import("./Routes/Home/Home.tsx"));
 const About = React.lazy(() => import("./Routes/About/About.tsx"));
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Navbar />
-      <Sidebar />
-      <div className="lg:ml-[20%] ml-0">
-        <Routes>
-          <Route>
-            <Route path="/" element={<App />} />
-            <Route
-              index
-              element={
-                <Suspense fallback={<Spinner />}>
-                  <Home />
-                </Suspense>
-              }
-            />
-            <Route
-              path="about"
-              element={
-                <Suspense fallback={<Spinner />}>
-                  <About />
-                </Suspense>
-              }
-            />
-            <Route
-              path="login"
-              element={
-                <Suspense fallback={<Spinner />}>
-                  <Login />
-                </Suspense>
-              }
-            />
-          </Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Navbar />
+        <Sidebar />
+        <div className="lg:ml-[20%] ml-0">
+          <Routes>
+            <Route>
+              <Route path="/" element={<App />} />
+              <Route
+                index
+                element={
+                  <Suspense fallback={<Spinner />}>
+                    <Home />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="about"
+                element={
+                  <Suspense fallback={<Spinner />}>
+                    <About />
+                  </Suspense>
+                }
+              />
+              <Route
+                path="login"
+                element={
+                  <Suspense fallback={<Spinner />}>
+                    <Login />
+                  </Suspense>
+                }
+              />
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </StrictMode>
 );

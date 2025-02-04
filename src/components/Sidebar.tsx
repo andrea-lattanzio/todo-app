c76459210d2d266011f4e1d5fcc62d4ad6e33501
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-02-04 17:12:33 +0100

    update sidebar style

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,62 +3,96 @@ import { NavLink } from "react-router-dom";
 const Sidebar: React.FC = () => {
   return (
     <>
-      <div className="fixed top-15 left-0 w-[20%] min-w-10 h-full bg-[#3c3d38] text-white p-6 hidden md:block">
-        <ul className="space-y-4">
+      <div className="fixed top-15 left-0 w-[20%] min-w-10 h-full bg-[#3c3d38] text-white p-4 hidden md:block">
+        <ul className="space-y-2 font-semibold">
           <li>
             <NavLink
               to="/"
               className={({ isActive }) => {
-                const baseStyles = "block p-2 rounded-lg hover:bg-[#31332e]";
+                const baseStyles =
+                  "block py-3 px-4 rounded-lg hover:bg-[#31332e]";
                 const activeStyles = isActive
                   ? "text-[#ffa552] font-semibold bg-[#31332e]"
                   : "hover:text-[#fcde9c]";
                 return `${baseStyles} ${activeStyles}`;
               }}
             >
-              Task List
+              <div className="flex items-center">
+                <i className="bi bi-house text-2xl mr-3"></i>
+                <p>Home</p>
+              </div>
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/add"
               className={({ isActive }) => {
-                const baseStyles = "block p-2 rounded-lg hover:bg-[#31332e]";
+                const baseStyles =
+                  "block py-3 px-4 rounded-lg hover:bg-[#31332e]";
                 const activeStyles = isActive
                   ? "text-[#ffa552] font-semibold bg-[#31332e]"
                   : "hover:text-[#fcde9c]";
                 return `${baseStyles} ${activeStyles}`;
               }}
             >
-              Add Task
+              <div className="flex items-center">
+                <i className="bi bi-file-earmark-plus text-2xl mr-3"></i>
+                Add Task
+              </div>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/categories"
+              className={({ isActive }) => {
+                const baseStyles =
+                  "block py-3 px-4 rounded-lg hover:bg-[#31332e]";
+                const activeStyles = isActive
+                  ? "text-[#ffa552] font-semibold bg-[#31332e]"
+                  : "hover:text-[#fcde9c]";
+                return `${baseStyles} ${activeStyles}`;
+              }}
+            >
+              <div className="flex items-center">
+                <i className="bi bi-folder text-2xl mr-3"></i>
+                <p>Categories</p>
+              </div>
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/tags"
               className={({ isActive }) => {
-                const baseStyles = "block p-2 rounded-lg hover:bg-[#31332e]";
+                const baseStyles =
+                  "block py-3 px-4 rounded-lg hover:bg-[#31332e]";
                 const activeStyles = isActive
                   ? "text-[#ffa552] font-semibold bg-[#31332e]"
                   : "hover:text-[#fcde9c]";
                 return `${baseStyles} ${activeStyles}`;
               }}
             >
-              Tags
+              <div className="flex items-center">
+                <i className="bi bi-tags text-2xl mr-3"></i>
+                <p>Tags</p>
+              </div>
             </NavLink>
           </li>
           <li>
             <NavLink
               to="/profile"
               className={({ isActive }) => {
-                const baseStyles = "block p-2 rounded-lg hover:bg-[#31332e]";
+                const baseStyles =
+                  "block py-3 px-4 rounded-lg hover:bg-[#31332e]";
                 const activeStyles = isActive
                   ? "text-[#ffa552] font-semibold bg-[#31332e]"
                   : "hover:text-[#fcde9c]";
                 return `${baseStyles} ${activeStyles}`;
               }}
             >
-              Profile
+              <div className="flex items-center">
+                <i className="bi bi-person-circle text-2xl mr-3"></i>
+                Profile
+              </div>
             </NavLink>
           </li>
         </ul>
@@ -77,7 +111,7 @@ const Sidebar: React.FC = () => {
                 return `${baseStyles} ${activeStyles}`;
               }}
             >
-              <i className="bi bi-list-check text-xl"></i>
+              <i className="bi bi-card-list text-xl"></i>
             </NavLink>
           </li>
           <li>
@@ -92,6 +126,20 @@ const Sidebar: React.FC = () => {
               }}
             >
               <i className="bi bi-plus-circle-fill text-xl"></i>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/categories"
+              className={({ isActive }) => {
+                const baseStyles = "block py-2 px-3 rounded-lg font-semibold";
+                const activeStyles = isActive
+                  ? "text-[#ffa552] font-semibold bg-[#31332e]"
+                  : "";
+                return `${baseStyles} ${activeStyles}`;
+              }}
+            >
+              <i className="bi bi-folder-fill text-xl"></i>
             </NavLink>
           </li>
           <li>

30932a875db13663b95359406bf11fb941b4c1db
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-02-09 15:38:59 +0100

    refactoring: refactor home component and liste component to handle no tasks use cases

--- src/Routes/Home/Home.tsx
+++ src/Routes/Home/Home.tsx
@@ -1,74 +1,41 @@
 import TaskList from "./components/TaskList/TaskList";
 import Spinner from "../../components/Spinner";
-import TaskDetail from "./components/TaskDetail";
-import deleteTask from "./api/deleteTask";
-import updateTask from "./api/updateTask";
 import useTasks from "./hooks/useTasks";
-import useTaskDetail from "./hooks/useTaskDetail";
-import useFilteredTasks from "./hooks/useFilteredTasks";
-import { useState } from "react";
-import NoTask from "./components/TaskList/NoTask";
+import { useMemo, useState } from "react";
 
 const Home = () => {
   const { tasks, loading } = useTasks();
-  const { detailedTask, setDetailedTask, toggleDetails } = useTaskDetail();
   const [showCompleted, setShowCompleted] = useState(false);
-  const filteredTasks = useFilteredTasks(tasks, showCompleted);
+  const filteredTasks = useMemo(() => {
+    return tasks.filter((task) => {
+      return showCompleted
+        ? task.status === "COMPLETED"
+        : task.status === "PENDING";
+    });
+  }, [tasks, showCompleted]);
 
   if (loading) return <Spinner />;
-  
+
   return (
     <div className="select-none overflow-auto h-[calc(100vh-100)]">
       <div className="flex flex-col items-center justify-center">
-        {detailedTask ? (
-          <div>
-            <button
-              type="button"
-              className="flex items-center bg-[#f88b25] text-[#292929] px-3 py-2 rounded-lg"
-              onClick={() => setDetailedTask(null)}
-            >
-              <i className="bi bi-arrow-left text-2xl font-extrabold"></i>
-              <span className="font-semibold">Back to list view</span>
-            </button>
-            <TaskDetail task={detailedTask} />
-            <div className="w-full flex justify-center gap-4 py-4 font-semibold">
-              <button
-                className="w-full sm:w-[40%] md:w-[30%] lg:w-[20%] px-6 py-2 bg-[#f88b25] text-[#292929] rounded-lg shadow-md transition-all duration-300 hover:bg-[#e67e22] active:scale-95"
-                onClick={() => updateTask(detailedTask.id, { name: "sas" })}
-              >
-                Mark as Done
-              </button>
-              <button
-                className="w-full sm:w-[40%] md:w-[30%] lg:w-[20%] px-6 py-2 bg-red-500 text-[#292929] rounded-lg shadow-md transition-all duration-300 hover:bg-red-600 active:scale-95"
-                onClick={() => deleteTask(detailedTask.id)}
-              >
-                Delete
-              </button>
-            </div>
+        <div className="h-96 w-full bg-[#2c2e2d] p-4 rounded-xl shadow-lg border-2 border-[#343434] flex flex-col items-center justify-center">
+          <TaskList
+            tasks={tasks}
+            filteredTasks={filteredTasks}
+            showCompleted={showCompleted}
+          />
+        </div>
+        {tasks.length > 0 && (
+          <div className="mt-3 flex items-center text-[#f88b25]">
+            <p className="font-semibold mr-4">Show completed tasks</p>
+            <i
+              className={`bi ${
+                showCompleted ? "bi-toggle-on" : "bi-toggle-off"
+              } text-4xl cursor-pointer`}
+              onClick={() => setShowCompleted((prev) => !prev)}
+            ></i>
           </div>
-        ) : (
-          <>
-            {tasks.length > 0 ? (
-              <>
-                <TaskList
-                  filteredTasks={filteredTasks}
-                  toggleDetails={toggleDetails}
-                  showCompleted={showCompleted}
-                />
-                <div className="mt-3 flex items-center text-[#f88b25]">
-                  <p className="font-semibold mr-4">Show completed tasks</p>
-                  <i
-                    className={`bi ${
-                      showCompleted ? "bi-toggle-on" : "bi-toggle-off"
-                    } text-4xl cursor-pointer`}
-                    onClick={() => setShowCompleted((prev) => !prev)}
-                  ></i>
-                </div>
-              </>
-            ) : (
-              <NoTask />
-            )}
-          </>
         )}
       </div>
     </div>
@@ -76,5 +43,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// {tasks.length ? <Spinner /> : <LoginForm onSubmitForm={onSubmit} error={error} />}
--- src/Routes/Home/components/TaskList/TaskList.tsx
+++ src/Routes/Home/components/TaskList/TaskList.tsx
@@ -1,14 +1,14 @@
-import { useNavigate } from "react-router-dom";
-import { TaskListItem, TaskListProps } from "../../types/task";
+import { NoTaskProps, TaskListProps } from "../../types/task";
 import NoTaskComponent from "./NoTaskComponent";
 import Task from "./Task";
 
-const getMessage = (showCompleted: boolean) => {
+const getMessage = (showCompleted: boolean) : NoTaskProps => {
   return showCompleted
     ? {
         name: "No completed tasks",
         description: "You have not completed any tasks yet. Keep going!",
         icon: "bi-list-check",
+        showCompleted: true
       }
     : {
         name: "All tasks completed!",
@@ -18,41 +18,32 @@ const getMessage = (showCompleted: boolean) => {
 };
 
 const TaskList: React.FC<TaskListProps> = ({
+  tasks,
   filteredTasks,
   showCompleted,
-  toggleDetails,
 }) => {
-  const navigate = useNavigate();
+  if (tasks.length === 0) {
+    const message: NoTaskProps = {
+      name: "No tasks available",
+      description: "Start by adding a new task to stay organized!",
+      icon: "bi-journal-plus",
+    };
+    return <NoTaskComponent {...message} />;
+  }
 
   if (filteredTasks.length === 0) {
     const message = getMessage(showCompleted);
-    return (
-      <div className="h-96 w-full bg-[#2c2e2d] p-4 rounded-xl shadow-lg border-2 border-[#343434] flex flex-col items-center justify-center">
-        <NoTaskComponent {...message} />
-        {!showCompleted && (
-          <button
-            className="mt-8 px-6 py-2 bg-[#f88b25] flex items-center text-gray-800 font-semibold rounded-lg shadow-md transition-all duration-300 hover:bg-[#e67e22] active:scale-95"
-            onClick={() => navigate("/add")}
-          >
-            <i className="bi bi-plus-circle mr-2 text-2xl"></i> Create Task
-          </button>
-        )}
-      </div>
-    );
+    return <NoTaskComponent {...message} />;
   }
 
   return (
-    <div className="h-96 w-full bg-[#2c2e2d] p-4 rounded-xl shadow-lg border-2 border-[#343434]">
+    <div className="w-full h-full">
       <div className="sticky top-0 z-10 flex-column p-2 font-semibold text-[#fbfbfb] text-xl bg-[#2c2e2d]">
         {showCompleted ? <p>Completed tasks</p> : <p>Your Tasks</p>}
       </div>
       <div className="mt-2 space-y-2 px-3 overflow-y-auto h-[calc(100%-3rem)] scrollbar-hidden pb-5">
         {filteredTasks.map((task) => (
-          <Task
-            key={task.id}
-            task={task}
-            onClick={(task: TaskListItem) => toggleDetails(task)}
-          />
+          <Task key={task.id} task={task} />
         ))}
       </div>
     </div>

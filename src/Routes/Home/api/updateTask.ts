b96d044b721541de8d05580e21ae75fdd996cefb
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-02-11 16:17:45 +0100

    integrated task detail with task api

--- src/Routes/Home/api/updateTask.ts
+++ src/Routes/Home/api/updateTask.ts
@@ -3,6 +3,7 @@ import authenticatedFetch from "../../../utils/auth.wrapper";
 class UpdateTaskDto {
   name?: string;
   description?: string;
+  status?: "PENDING" | "COMPLETED";
 }
 
 export default async function updateTask(taskId: string, body: UpdateTaskDto) {
--- src/Routes/Home/components/TaskDetail.tsx
+++ src/Routes/Home/components/TaskDetail.tsx
@@ -3,7 +3,12 @@ import { useNavigate, useParams } from "react-router-dom";
 import { TaskListItem } from "../types/task";
 import getTaskById from "../api/getTaskById";
 import Spinner from "../../../components/Spinner";
-import { priorityColors, statusColors } from "../../../components/ui/taskColors";
+import {
+  priorityColors,
+  statusColors,
+} from "../../../components/ui/taskColors";
+import deleteTask from "../api/deleteTask";
+import updateTask from "../api/updateTask";
 
 const TaskDetail = () => {
   const { taskId } = useParams();
@@ -23,12 +28,29 @@ const TaskDetail = () => {
     }
   };
 
+  const handleClick = async (deleting?: boolean) => {
+    if (!task) return;
+    try {
+      setLoading(true);
+      deleting
+        ? await deleteTask(task.id)
+        : updateTask(task.id, { status: "COMPLETED" });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+      navigate("/");
+    }
+  };
+
   useEffect(() => {
     if (!taskId) return;
     getTask(taskId);
   }, []);
 
-  return loading || !task ? (
+  if (!task) return;
+
+  return loading ? (
     <Spinner />
   ) : (
     <>
@@ -41,7 +63,9 @@ const TaskDetail = () => {
       </button>
       <div className="select-none mt-3 h-auto w-full bg-[#2c2e2d] p-6 rounded-xl shadow-lg border-2 border-[#343434]">
         <div className="flex items-center justify-between">
-          <h2 className="font-semibold text-[#6b7280] text-2xl">Task Details</h2>
+          <h2 className="font-semibold text-[#6b7280] text-2xl">
+            Task Details
+          </h2>
           <button className="h-10 w-10 flex items-center justify-center p-2 bg-[#697565] text-gray-800 rounded-full transition-all duration-300 hover:bg-[#5a6456] active:scale-95">
             <i className="bi bi-pencil text-xl"></i>
           </button>
@@ -59,7 +83,9 @@ const TaskDetail = () => {
 
           {/* Task Due Date */}
           <div className="flex flex-col">
-            <label className="text-[#656567]  fontfont-semibold">Due Date</label>
+            <label className="text-[#656567]  fontfont-semibold">
+              Due Date
+            </label>
             <span className="mt-2 text-gray-400 font-medium">
               {new Date(task.dueDate).toLocaleDateString("en-US", {
                 weekday: "short",
@@ -99,7 +125,9 @@ const TaskDetail = () => {
           {/* Task Categories */}
           {task.categories!.length > 0 && (
             <div className="flex flex-col">
-              <label className="text-[#656567]  font-semibold">Categories</label>
+              <label className="text-[#656567]  font-semibold">
+                Categories
+              </label>
               <span className="mt-2 text-gray-400 font-medium">
                 {task.categories!.map((cat, index) => (
                   <span key={index}>
@@ -112,14 +140,19 @@ const TaskDetail = () => {
           )}
         </div>
       </div>
-      <div className="mt-8 flex flex-col items-center justify-between w-full h-auto space-y-2">
+      <div className="mt-5 flex flex-col items-center justify-between w-full h-auto space-y-2">
+        {task.status !== "COMPLETED" && (
+          <button
+            className="text-center lg:w-60 w-full py-2 bg-[#697565] flex items-center justify-center text-gray-800 font-semibold rounded-lg shadow-md transition-all duration-300 hover:bg-[#5a6456] active:scale-95"
+            onClick={() => handleClick()}
+          >
+            Mark as completed
+          </button>
+        )}
         <button
-          className="text-center lg:w-60 w-full py-2 bg-[#697565] flex items-center justify-center text-gray-800 font-semibold rounded-lg shadow-md transition-all duration-300 hover:bg-[#5a6456] active:scale-95"
-          onClick={() => navigate("/")}
+          className="text-center lg:w-60 w-full py-2 bg-[#f88b25] flex items-center justify-center text-gray-800 font-semibold rounded-lg shadow-md transition-all duration-300 hover:bg-[#e67e22] active:scale-95"
+          onClick={() => handleClick(true)}
         >
-          Mark as completed
-        </button>
-        <button className="text-center lg:w-60 w-full py-2 bg-[#f88b25] flex items-center justify-center text-gray-800 font-semibold rounded-lg shadow-md transition-all duration-300 hover:bg-[#e67e22] active:scale-95">
           Delete
         </button>
       </div>
